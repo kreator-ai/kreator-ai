fc7e0ad101029681006c495df5d79facc8bd472e
Vladymyr12 <yvladimir343@gmail.com>
2024-08-08 18:19:28 -0600

    vercel env add

--- src/lib/gemini-ai.ts
+++ src/lib/gemini-ai.ts
@@ -1,6 +1,12 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
+
 const apiKey = process.env.GEMINI_API_KEY;
+
+if (!apiKey) {
+  throw new Error('GEMINI_API_KEY is not defined');
+}
+
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
